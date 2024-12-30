b04283a49080bc45aad77a5a9dfbd7794ec9da1f
Sahil Roy <101969645+freneticdemigod@users.noreply.github.com>
2024-12-30 20:40:38 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,8 +3,7 @@ import os
 
 load_dotenv()
 
-# database_url = os.getenv('SQLALCHEMY_DATABASE_URI')
-database_url = 'postgresql://postgres:sahilR%4005@localhost:5432/task_management'
+database_url = os.getenv('SQLALCHEMY_DATABASE_URI')
 class Config:
     SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
